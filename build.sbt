2360e43275e0ef22b95c568bc704e157402dd951
Gerd Reiss <gerdreiss@@users.noreply.github.com>
2022-07-05 18:44:58 +0200

    [Exploring ZIO & Tapir & OpenAPI] still 404

--- build.sbt
+++ build.sbt
@@ -5,11 +5,11 @@ val tapirVersion = "1.0.1"
 lazy val root = project
   .in(file("."))
   .settings(
-    name := "explore-zio-tapir-openapi",
-    version := "0.1.0-SNAPSHOT",
+    name         := "explore-zio-tapir-openapi",
+    version      := "0.1.0-SNAPSHOT",
     scalaVersion := scala3Version,
     libraryDependencies ++= Seq(
-      "com.softwaremill.sttp.tapir" %% "tapir-zio-http-server" % tapirVersion,
+      "com.softwaremill.sttp.tapir" %% "tapir-zio-http-server"   % tapirVersion,
       "com.softwaremill.sttp.tapir" %% "tapir-swagger-ui-bundle" % tapirVersion
     )
   )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -20,19 +20,18 @@ object Main extends ZIOAppDefault:
   val lengthEndpoint: PublicEndpoint[String, Unit, Int, Any] =
     endpoint.in("length").post.in(stringBody).out(plainBody[Int])
 
+  val serverEndpoints: List[ServerEndpoint[Any, Task]] =
+    List(
+      helloEndpoint.zServerLogic(_ => ZIO.succeed("Hello, world!")),
+      lengthEndpoint.zServerLogic(input => ZIO.succeed(input.length))
+    )
+
   val swaggerEndpoints: List[ServerEndpoint[Any, Task]] =
     SwaggerInterpreter()
-      .fromServerEndpoints[Task](
-        List(
-          helloEndpoint.zServerLogic(_ => ZIO.succeed("Hello, world!")),
-          lengthEndpoint.zServerLogic(input => ZIO.succeed(input.length))
-        ),
-        "ZIO Tapir OpenAPI Explorer",
-        "1.0"
-      )
+      .fromServerEndpoints[Task](serverEndpoints, "ZIO Tapir OpenAPI Explorer", "1.0")
 
-  val swaggerRoute: Http[Any, Throwable, Request, Response] =
+  val httpApp: Http[Any, Throwable, Request, Response] =
     ZioHttpInterpreter().toHttp(swaggerEndpoints)
 
   override def run: ZIO[Any & (ZIOAppArgs & Scope), Any, Any] =
-    Server.start(8080, swaggerRoute)
+    Server.start(8080, httpApp)
